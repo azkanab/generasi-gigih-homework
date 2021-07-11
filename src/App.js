6deb3ed8ce3c76187358027306162ef5730e2526
azkanab <azkanabilah@gmail.com>
2021-07-12 00:21:46 +0700

    Add fake spotify key (real spotify key is on local)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(process.env.REACT_APP_SPOTIFY_KEY)
+  
   return (
     <div className="App">
       <header className="App-header">
