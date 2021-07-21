8d7c620aabecaa18cd31c3cc027246ae45b8be2c
azkanab <azkanabilah@gmail.com>
2021-07-21 08:54:51 +0700

    Handle logo navbar click

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,23 +1,36 @@
 import '../../styles/Navbar/Navbar.css'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useHistory } from 'react-router-dom'
 import { getUserState } from '../../state/user'
+import { myTokenState } from '../../state/auth/token'
 import { useRecoilValue } from 'recoil'
 import Image from '../common/Image'
 import Text from '../common/Text'
+import isObjectEmpty from '../../utils/isObjectEmpty'
 
 export default function Navbar() {
     const user = useRecoilValue(getUserState)
+    const token = useRecoilValue(myTokenState)
     const LOGO_IMG_URL = './spotify.png'
     const LOGO_TEXT = 'Spotifi'
     let location = useLocation()
+    let history = useHistory()
 
     const hideProfile = () => {
         return location.pathname === '/login'
     }
 
+    const handleLogoClick = () => {
+        console.log("click")
+        if (isObjectEmpty(token)) {
+            history.push("/login")
+        } else {
+            history.go(0)
+        }
+    }
+
     return (
         <div className="header">
-            <div className="logo">
+            <div className="logo" onClick={handleLogoClick}>
                 <Image imgUrl={LOGO_IMG_URL} imgClass="logo-img" />
                 <Text text={LOGO_TEXT} textClass="header-logo-text" />
             </div>
--- src/components/common/Loader/index.js
+++ src/components/common/Loader/index.js
@@ -5,7 +5,7 @@ export default function Loader() {
     return (
         <div className="loader-container">
             <div>
-                <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+                <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
                 <Text text="Loading..." textClass="loading" />
             </div>
         </div>
