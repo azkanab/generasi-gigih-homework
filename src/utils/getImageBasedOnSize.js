a36aedd540e1aab14256438e72512cafbf19f493
azkanab <azkanabilah@gmail.com>
2021-07-25 12:35:32 +0700

    Change basic image url on getImageBasedOnSize

--- src/utils/getImageBasedOnSize.js
+++ src/utils/getImageBasedOnSize.js
@@ -1,4 +1,4 @@
-const BASIC_IMG_URL = '/smiley.png'
+const BASIC_IMG_URL = '/unknown_playlist.png'
 
 export default function getImageBasedOnSize(images, size) {
     return images.find(image => { return image.width === size }) || BASIC_IMG_URL
